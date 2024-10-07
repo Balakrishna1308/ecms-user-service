0fce8ca2775211b1e5463b166fdfca45166466a1
Bala <balakrishna.malipeddi@gmail.com>
2024-10-07 19:13:22 +0530

    Added initial User Service implementation.

---
+++ src/main/java/com/myshow4all/ecms_user_service/EcmsUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.myshow4all.ecms_user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcmsUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcmsUserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myshow4all/ecms_user_service/EcmsUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myshow4all.ecms_user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcmsUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
